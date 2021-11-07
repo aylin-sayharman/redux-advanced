283200dfb5367e8160e751be4a113744d087beeb
Aylin Sayharman <aylin.sayharman@gmail.com>
2021-11-07 23:17:33 +0300

    add http request and action creator

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, Fragment } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
+
   return (
-    <Layout>
+    <Fragment>
+      {notification && 
+        <Notification 
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      }
+      <Layout>
       {showCart && <Cart />}
       <Products />
     </Layout>
+    </Fragment>
+    
   );
 }
 
